1bdb1aa3c67af633b127de6d41374665bcf72be9
Daniz <dani.tr7777@gmail.com>
2024-07-28 23:17:46 +0330

    intial& add customuser model

---
+++ accounts/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
+from accounts.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('email', 'username',)
+
+
