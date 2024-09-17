2e1a9528245b9f80a5d39fd88bee00efd512f267
Daniz <dani.tr7777@gmail.com>
2024-09-17 23:33:03 +0330

    add static files

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import Product
+from django.views import generic
 
-# Create your views here.
+
+class ProductListView(generic.ListView):
+    queryset = Product.objects.filter(status=True)
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
