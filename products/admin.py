a3c23a90f8d87ef0c4f80665331658d181cc5025
Daniz <dani.tr7777@gmail.com>
2024-10-06 22:36:41 +0330

    add translation to products app

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active',]
+    extra = 0
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'status')
+
+    inlines = [
+        CommentInline,
+    ]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('product', 'author', 'body', 'stars', 'active')
+
+
+
+
 
--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 from django.shortcuts import reverse
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -16,3 +18,34 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[str(self.id)])
+
+
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
+
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', _('Very Bad')),
+        ('2', _('Bad')),
+        ('3', _('Normal')),
+        ('4', _('Good')),
+        ('5', _('Perfect')),
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField(verbose_name=_('comment text'))
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('rate'))
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    active = models.BooleanField(default=True)
+
+    # Manager
+    objects = models.Manager()
+    active_comments = ActiveCommentsManager()
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.product.id])
